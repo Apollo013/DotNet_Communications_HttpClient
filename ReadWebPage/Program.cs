f742428bbf84974796e0b9bb8ab5cf005fe8ace2
Paul <paul.j.millar.1971@gmail.com>
2016-10-05 21:25:43 +0100

    Added web page reader example

---
+++ ReadWebPage/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace ReadWebPage
+{
+    /// <summary>
+    /// This example simply reads a page from the bcc home website
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Uri uri = new Uri("http://bbc.co.uk");
+            Task.Factory.StartNew(() => GetPage(uri));
+            Console.ReadKey();
+        }
+
+        private static async Task GetPage(Uri uri)
+        {
+            HttpClient client = new HttpClient();
+            string page = await client.GetStringAsync(uri);
+            Console.WriteLine(page);
+        }
+    }
+}
