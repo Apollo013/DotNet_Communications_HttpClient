b77a423fd06f831bf2ef08ede34b319e5f39fec6
Paul <paul.j.millar.1971@gmail.com>
2016-10-05 22:17:53 +0100

    Started Web Api Example

---
+++ WebApiExample.Server/App_Start/WebApiConfig.cs
@@ -0,0 +1,23 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using System.Web.Http;
+
+namespace WebApiExample.Server
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            config.MapHttpAttributeRoutes();
+
+            CondigureMessageFormat(config);
+        }
+
+        private static void CondigureMessageFormat(HttpConfiguration config)
+        {
+            var formatter = config.Formatters.JsonFormatter;
+            formatter.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();   // Use camel case
+            formatter.SerializerSettings.ReferenceLoopHandling = ReferenceLoopHandling.Ignore;              // Ignore circular references
+        }
+    }
+}
---
+++ WebApiExample.Server/Startup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+using System.Web.Http;
+
+[assembly: OwinStartup(typeof(WebApiExample.Server.Startup))]
+
+namespace WebApiExample.Server
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            HttpConfiguration config = new HttpConfiguration();
+
+
+            app.UseWebApi(config);
+        }
+    }
+}
